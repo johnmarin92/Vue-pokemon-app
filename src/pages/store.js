0a0aa17eed85014ca1d183c46f173198b316138d
John marin <u922857@proveedor.teco.com.ar>
2022-07-10 22:00:38 -0300

    actualizacion final

--- src/pages/store.js
+++ src/pages/store.js
@@ -1,4 +1,5 @@
 import { reactive, readonly } from "vue";
+import { Notify } from 'quasar'
 
 const state = reactive({
   data: [],
@@ -13,20 +14,42 @@ const actions = {
     state.description = d;
     console.log("setDescription:", state.description);
   },
+  //FUNCION QUE TRAE LISTA DE POKEMONS
   async getPokemons() {
     state.data = [];
-    fetch("https://pokeapi.co/api/v2/pokemon/")
-      .then((response) => response.json())
-      .then((data) => actions.getPokemonsInfo(data.results));
+    fetch("https://pokeapi.co/api/v2/pokemonn/")
+    .then((response) => response.json())
+    .then((data) => actions.getPokemonsInfo(data.results))
+    .catch(function(error) {
+      Notify.create({
+        message: 'Error de conexión con el Servidor',
+        color: 'red-12',
+        textColor: 'white',
+        icon: 'error'
+      })
+    });
   },
+  //FUNCION QUE RECIBE LISTA DE POKEMONS Y CONSULTA PARA CADA UNO SU INFORMACION
   getPokemonsInfo(pokemons) {
     for (const pokemon of pokemons) {
       let url = `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`;
       fetch(url)
         .then((response) => response.json())
-        .then((data) => actions.getPokemonSpecies(data));
+        .then((data) => actions.getPokemonSpecies(data))
+        .catch(function(error) {
+          Notify.create({
+            message: 'Error de conexión con el Servidor',
+            color: 'red-12',
+            textColor: 'white',
+            icon: 'error'
+          })
+        });
     }
   },
+  /*
+    FUNCION QUE CONSULTA DESCRIPCION DE POKEMON Y POSTERIORMENTE GUARDA TODO EL ARRAY DE POKEMONS
+    CON SU DESCRIPCION EN ESPAÑOL
+  */
   getPokemonSpecies(pokemon) {
     fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon.id}`)
       .then((response) => response.json())
@@ -35,7 +58,15 @@ const actions = {
           pokemon,
           pokemonSpecie.flavor_text_entries
         )
-      );
+      )
+      .catch(function(error) {
+        Notify.create({
+          message: 'Error de conexión con el Servidor',
+          color: 'red-12',
+          textColor: 'white',
+          icon: 'error'
+        })
+      });
   },
   getPokemonDescription(pokemon, flavorTextEntries) {
     for (let entri of flavorTextEntries) {
